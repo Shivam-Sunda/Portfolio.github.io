f6accb4ad0fd8fdb01f31c1885d8db4756278e6b
Shivam-Sunda <157489840+Shivam-Sunda@users.noreply.github.com>
2024-03-07 03:08:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-document.getElementById('contact-button').addEventListener('click', function() {
-    var contactForm = document.getElementById('contact-form');
-    if (contactForm.style.display === 'none') {
-        contactForm.style.display = 'block';
-    } else {
-        contactForm.style.display = 'none';
+function show() {
+    document.getElementById('bar').classList.toggle('active');
+}
+
+var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
     }
+
+    
+var user_name = "";
+var email = "";
+var message = "";
+
+function submit(event) {
+    event.preventDefault();
+    var contact = document.getElementById('contact');
+    contact.classList.toggle('active');
+
+    user_name = document.querySelector('#contact .hello[name="Name"]').value;
+    email = document.querySelector('#contact .hello[name="email"]').value;
+    message = document.querySelector('#contact textarea.hello').value;
+    
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#contact form').addEventListener('submit', function(event) {
+        submit(event);
+        console.log(user_name,email,message);
+        alert("Form submit successful");
+    });
 });
-
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         console.log(entry)
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('show');
-//         } else {
-//             entry.target.classList.remove('show');
-//         }
-//     });
-// });
-
-// const hiddenElements = document.querySelectorAll('.hidden');
-// hiddenElements.forEach((el) => observer.observe(el));
